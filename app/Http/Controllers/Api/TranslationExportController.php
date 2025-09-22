3e972f7fe18c8e505cc1e041fe92187916a25a46
Hamza Saleem <hamza313079@gmail.com>
2025-09-22 17:45:04 +0500

    fix

--- app/Http/Controllers/Api/TranslationExportController.php
+++ app/Http/Controllers/Api/TranslationExportController.php
@@ -5,24 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\TranslationKey;
-use App\Services\TranslationVersion;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Arr;
 
 class TranslationExportController extends Controller
 {
-
     public function export(Request $request)
     {
-
-        
         $locales = $request->query('locale'); 
         $tags    = $request->query('tags');
 
-        
-        $query = TranslationKey::with(['translations.locale', 'tags']);
+        $query = TranslationKey::query()->select('id', 'key', 'description');
 
-        
+        // --- Filter by tags ---
         if (!empty($tags)) {
             $tagArray = array_map('trim', explode(',', $tags));
             $query->whereHas('tags', function ($q) use ($tagArray) {
@@ -30,31 +23,51 @@ class TranslationExportController extends Controller
             });
         }
 
-        $keys = $query->get();
+        // --- Eager load relationships ---
+        if ($locales) {
+            $allowedLocales = array_map('trim', explode(',', $locales));
 
-      
-        $result = [];
-        $allowedLocales = $locales ? explode(',', $locales) : null;
+            $query->whereHas('translations.locale', function ($q) use ($allowedLocales) {
+                $q->whereIn('code', $allowedLocales);
+            });
 
-        foreach ($keys as $key) {
-            foreach ($key->translations as $translation) {
-                $code = $translation->locale->code;
-
-                
-                if ($allowedLocales && !in_array($code, $allowedLocales)) {
-                    continue;
-                }
-
-                if (!isset($result[$code])) {
-                    $result[$code] = [];
-                }
-
-                $result[$code][$key->key] = $translation->content;
-            }
+            $query->with([
+                'translations' => function ($q) use ($allowedLocales) {
+                    $q->select('id', 'translation_key_id', 'locale_id', 'content')
+                      ->whereHas('locale', function ($sub) use ($allowedLocales) {
+                          $sub->whereIn('code', $allowedLocales);
+                      });
+                },
+                'translations.locale:id,code,name',
+                'tags:id,name'
+            ]);
+        } else {
+            $query->with([
+                'translations:id,translation_key_id,locale_id,content',
+                'translations.locale:id,code,name',
+                'tags:id,name'
+            ]);
         }
 
-        return response()->json($result);
+        $keys = $query->get();
+
+        // --- Clean response ---
+        $keys->transform(function ($item) {
+            // hide foreign keys in translations
+            $item->translations->each(function ($t) {
+                $t->makeHidden(['translation_key_id', 'locale_id']);
+            });
+
+            // hide pivot data in tags
+            $item->tags->each(function ($tag) {
+                $tag->makeHidden(['pivot']);
+            });
+
+            return $item;
+        });
+
+        return response()->json([
+            'data' => $keys
+        ]);
     }
-
-
 }
